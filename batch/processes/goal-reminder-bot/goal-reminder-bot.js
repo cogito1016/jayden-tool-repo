1013697ae676bac4d9e0425ecce0d89ba1064ed8
cogito1016 <cogito1016@naver.com>
2024-05-12 12:01:52 +0900

    test: 테스트

--- batch/processes/goal-reminder-bot/goal-reminder-bot.js
+++ batch/processes/goal-reminder-bot/goal-reminder-bot.js
@@ -1,6 +1,9 @@
 import { MemberObjectList } from "./MemberObjectList.js";
 
 export function goalReminderProcess() {
+  sendMessageToTestChannel({
+    text: `리마인더봇 배치시간이되어 작동합니다`,
+  });
   const webhook = process.env.GOAL_STUDY_SLACK_WEBHOOK;
   console.log(webhook);
 
