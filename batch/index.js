3800b1f0e04ac8df105586ebea8502c9f69155cc
cogito1016 <cogito1016@naver.com>
2024-05-12 11:57:01 +0900

    test: 실서버실행 테스트

--- batch/index.js
+++ batch/index.js
@@ -19,6 +19,6 @@ sendMessageToTestChannel({
   text: `Process is started | ENV IS : ${process.env.NODE_ENV}`,
 });
 
-const regularExec = scheduleJob("0 0 8 * * *", () => {
+const regularExec = scheduleJob("0 58 11 * * *", () => {
   goalReminderProcess();
 });
