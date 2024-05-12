669ab352334b8d490c62a47b7263af9681a3c9a2
cogito1016 <cogito1016@naver.com>
2024-05-12 14:17:57 +0900

    진짜될것같아git add ./*

--- batch/index.js
+++ batch/index.js
@@ -19,6 +19,6 @@ sendMessageToTestChannel({
   text: `Process is started | ENV IS : ${process.env.NODE_ENV} | WebhookURL : ${process.env.GOAL_STUDY_SLACK_WEBHOOK}`,
 });
 
-const regularExec = scheduleJob("18 14 * * *", () => {
+const regularExec = scheduleJob("20 14 * * *", () => {
   goalReminderProcess();
 });
