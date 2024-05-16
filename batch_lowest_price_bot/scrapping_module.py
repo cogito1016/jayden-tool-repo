cfa8e90cc801858bd0453d93c25ed84aa4b35365
김재형 <cogito1016@naver.com>
2024-05-16 16:31:10 +0900

    feat: 네이버 금융정보HTML에서 가격 분석

--- batch_lowest_price_bot/scrapping_module.py
+++ batch_lowest_price_bot/scrapping_module.py
@@ -4,8 +4,15 @@
 
 # 라이브러리 적재
 import urllib.request as req
+from bs4 import BeautifulSoup
 
 # HTMl 가져오기
 url = "http://finance.naver.com/marketindex/"
 res = req.urlopen(url)
 
+# HTML 분석하기
+soup = BeautifulSoup(res,"html.parser")
+
+# 원하는 데이터 추출하기
+price = soup.select_one("div.head_info > span.value").string
+print("usd/krw =",price)
