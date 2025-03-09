8682ea0e969e64488c9beea84f3d32bc4575467b
cogito1016 <cogito1016@naver.com>
2025-03-09 16:44:42 +0900

    chore: URL검증시에도 콘솔ㅊㄹ력

--- laboratory/apps/api/src/api.service.ts
+++ laboratory/apps/api/src/api.service.ts
@@ -8,6 +8,8 @@ import {
 export class ApiService {
   processUserReponse(webhookData: SlackWebhookEvent) {
     if (webhookData.type === URL_VERIFICATION) {
+      console.log('URL 검증 이벤트');
+      console.log(webhookData.challenge);
       return webhookData.challenge;
     }
 
